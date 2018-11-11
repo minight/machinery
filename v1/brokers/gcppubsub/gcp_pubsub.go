4a94e491f8946c99d08f0c6897e0d61a4b1e2832
esswhy <s@mewy.pw>
2018-11-11 23:19:35 +1100

    Renamed everything

--- v1/brokers/gcppubsub/gcp_pubsub.go
+++ v1/brokers/gcppubsub/gcp_pubsub.go
@@ -10,11 +10,11 @@ import (
 	"time"
 
 	"cloud.google.com/go/pubsub"
-	"github.com/RichardKnop/machinery/v1/brokers/iface"
-	"github.com/RichardKnop/machinery/v1/common"
-	"github.com/RichardKnop/machinery/v1/config"
-	"github.com/RichardKnop/machinery/v1/log"
-	"github.com/RichardKnop/machinery/v1/tasks"
+	"github.com/minight/machinery/v1/brokers/iface"
+	"github.com/minight/machinery/v1/common"
+	"github.com/minight/machinery/v1/config"
+	"github.com/minight/machinery/v1/log"
+	"github.com/minight/machinery/v1/tasks"
 )
 
 // Broker represents an Google Cloud Pub/Sub broker
